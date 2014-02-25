c2cd8c83c48752197f7d9f8ff0886b943ccd6dc8
Sebastian Gil Parga <s.gilparga@gmail.com>
2014-02-25 17:25:10 -0500

    Se puede leer un archivo de texto que este en la carpeta data

---
+++ CyberAventura/Assets/Scripts/IOPreguntas.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class IOPreguntas{
+	string contenido;
+
+	public IOPreguntas(string nArchivo){
+		Debug.Log(Application.dataPath + "/" + nArchivo);
+		StreamReader sr = new StreamReader(Application.dataPath + "/" + nArchivo);
+		contenido  = sr.ReadToEnd();
+	}
+
+	public string DarContenido(){
+		return contenido;
+	}
+}
---
+++ CyberAventura/Assets/Scripts/PruebaIO.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PruebaIO : MonoBehaviour {
+
+	IOPreguntas p;
+	// Use this for initialization
+	void Start () {
+		p = new IOPreguntas("data/prueba.txt");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		GUI.Label(new Rect(10,10,100,100),p.DarContenido());
+	}
+}
