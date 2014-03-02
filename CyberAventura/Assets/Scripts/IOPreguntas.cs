00f28efaa88d16373caf64e689b061945adb8e56
Sebastian Gil Parga <s.gilparga@gmail.com>
2014-03-02 18:48:19 -0500

    Visualizacion del panel de preguntas

--- CyberAventura/Assets/Scripts/IOPreguntas.cs
+++ CyberAventura/Assets/Scripts/IOPreguntas.cs
@@ -2,19 +2,36 @@
 using System.Collections;
 using System.IO;
 
+/*
+ * Clase que permite acceder al texto de las preguntas guardadas en un archivo de texto
+ * */
 public class IOPreguntas{
-	private string 		archivo;
-	private int 		numPreguntas;
-	private Pregunta[]	listaPreguntas;
+	//------------------------------------------------------------------------------------------
+	// Atributos
+	//------------------------------------------------------------------------------------------
+
+	private string 		archivo;		//Nombre del archivo que se va a cargar
+	private int 		numPreguntas;	//Numero de preguntas en el archivo
+	private Pregunta[]	listaPreguntas;	//Lista de las preguntas cargadas
+	private Pregunta	preguntaActual;	//Pregunta de la que actualmente setiene informacion
+	private int			indiceActual;	//Indice de la pregunta actual
+
+	//------------------------------------------------------------------------------------------
+	// Constructor
+	//------------------------------------------------------------------------------------------
 
 	public IOPreguntas(string nArchivo){
 		archivo = nArchivo;
 		CargarPreguntas();
+		indiceActual = 0;
+		preguntaActual = listaPreguntas[indiceActual];
 	}
 
+	// Parsea el archivo de texto y recupera las preguntas
 	private void CargarPreguntas(){
-		StreamReader sr = new StreamReader(Application.dataPath + "/Data/" + archivo);
+		StreamReader sr = new StreamReader(Application.dataPath + "/Data/" + archivo + ".txt");
 		string contenido  = sr.ReadToEnd();
+		sr.Close();
 		string[] data = contenido.Split('\n');
 		numPreguntas = System.Int32.Parse(data[0]);
 		listaPreguntas = new Pregunta[numPreguntas];
@@ -25,7 +42,45 @@ public class IOPreguntas{
 		}
 	}
 
-	public string Prueba(){
-		return listaPreguntas[3].DarOpcionC();
+	//-------------------------------------------------------------------------------------------
+	// Metodos
+	//-------------------------------------------------------------------------------------------
+
+	// Metodos para recuperar la informacion dela pregunta actual
+	public string DarTexto(){
+		return preguntaActual.DarTexto();
+	}
+
+	public string DarOpcionA(){
+		return preguntaActual.DarOpcionA();
+	}
+
+	public string DarOpcionB(){
+		return preguntaActual.DarOpcionB();
+	}
+
+	public string DarOpcionC(){
+		return preguntaActual.DarOpcionC();
+	}
+
+	public string DarOpcionD(){
+		return preguntaActual.DarOpcionD();
+	}
+
+	//Cambia la pregunta actual a la siguiente en la lista
+	public void AvanzarPregunta(){
+		indiceActual++;
+		preguntaActual = listaPreguntas[indiceActual]; 
+	}
+
+	//Verifica si hay otra pregunta para cargar
+	public bool HaySiguiente(){
+		if(indiceActual < numPreguntas)
+			return true;
+		return false;
+	}
+
+	public bool ValidarRespuesta(string Respuesta){
+		return preguntaActual.ValidarRespuesta(Respuesta);
 	}
 }
---
+++ CyberAventura/Assets/Scripts/PanelPregunta.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelPregunta : MonoBehaviour {
+
+	private	IOPreguntas listaPreguntas;
+	private Rect 		WindowRect;
+	private string		textoActual;
+	private string 		AActual;
+	private string		BActual;
+	private string		CActual;
+	private string		DActual;
+
+	void Start () {
+		textoActual = "";
+		AActual = "";
+		BActual = "";
+		CActual = "";
+		DActual = "";
+
+		CargarLista("prueba");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnGUI(){
+		WindowRect = new Rect(50,50,Screen.width - 100,Screen.height - 100);
+		WindowRect = GUI.Window(0,WindowRect,WindowFunction,"");
+	}
+
+	void WindowFunction(int WindowID){
+		GUI.Label(new Rect(50,50,(WindowRect.width/2) - 50,WindowRect.height - 50),textoActual);
+		GUI.Button(new Rect(WindowRect.width/2,50,WindowRect.width/2 - 50,WindowRect.height/8),AActual);
+		GUI.Button(new Rect(WindowRect.width/2,50 + 2*(WindowRect.height/8),WindowRect.width/2 - 50,WindowRect.height/8),BActual);
+		GUI.Button(new Rect(WindowRect.width/2,50 + 4*(WindowRect.height/8),WindowRect.width/2 - 50,WindowRect.height/8),CActual);
+		GUI.Button(new Rect(WindowRect.width/2,50 + 6*(WindowRect.height/8),WindowRect.width/2 - 50,WindowRect.height/8),DActual);
+	}
+
+	void CargarLista(string NombreLista){
+		listaPreguntas = new IOPreguntas(NombreLista);
+		MostrarPregunta();
+	}
+
+	bool CambiarPregunta(){
+		if(listaPreguntas.HaySiguiente()){
+			listaPreguntas.AvanzarPregunta();
+			MostrarPregunta();
+			return true;
+		}
+		return false;
+	}
+
+	void MostrarPregunta(){
+		textoActual = listaPreguntas.DarTexto();
+		AActual = listaPreguntas.DarOpcionA();
+		BActual = listaPreguntas.DarOpcionB();
+		CActual = listaPreguntas.DarOpcionC();
+		DActual = listaPreguntas.DarOpcionD();
+	}
+}
